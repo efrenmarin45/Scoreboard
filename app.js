79f4287dd66e9eff3cbdee3a6d9f3bbcfeff2498
Efren_Marin <efren45marin@gmail.com>
2020-10-04 20:44:21 -0700

    Set up compondngs, CSS, and HTML to display a static scoreboard

---
+++ app.js
@@ -0,0 +1,80 @@
+const players = [
+    {
+        name: "D.VA",
+        score: 75,
+        id: 1
+    },
+    {
+        name: "Zenyatta",
+        score: 50,
+        id: 2
+    },
+    {
+        name: "Soldier 76",
+        score: 35,
+        id: 3
+    },
+    {
+        name: "Sombra",
+        score: 25,
+        id: 4
+    },
+]
+
+const Header = (props) => {
+    return(
+        <header>
+            <h1> {props.title} </h1>
+            <span className='stats'>Players: {props.totalPlayers}</span>
+        </header>
+    );
+};
+
+const Player = (props) => {
+    return(
+        <div className='player'>
+            <span className='player-name'>
+                {props.name}
+            </span>
+            <Counter 
+                score={props.score}
+            />
+        </div>
+    );
+};
+
+const Counter = (props) => {
+    return(
+        <div className='counter'>
+            <button className='counter-action decrement'> - </button>
+            <span className='counter-score'> {props.score} </span>
+            <button className='counter-action increment'> + </button>
+        </div>
+    )
+}
+
+
+const App = (props) => {
+    return(
+        <div className='scoreboard'>
+            <Header 
+            title="Scoreboard" 
+            totalPlayers={props.initialPlayers.length}
+            />
+
+            {/* List of Players */}
+            {props.initialPlayers.map(player =>
+                <Player 
+                    name={player.name} 
+                    score={player.score}
+                    key={player.id}
+                />
+            )}
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App initialPlayers={ players }/>,
+    document.getElementById('root')
+);
