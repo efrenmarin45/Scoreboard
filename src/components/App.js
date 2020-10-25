1a2d59684103c14cf0c3697970bc159c952404f5
Efren_Marin <efren45marin@gmail.com>
2020-10-25 14:57:14 -0700

    Created separate component files within the components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,6 @@
-import React from 'react';
-
-const Header = (props) => {
-    return(
-        <header>
-            <h1> {props.title} </h1>
-            <span className='stats'>Players: {props.totalPlayers}</span>
-        </header>
-    );
-};
-
-
-const Player = (props) => {
-    return(
-        <div className='player'>
-            <span className='player-name'>
-                <button className='remove-player' onClick={() => props.removePlayer(props.id)}>✖</button>
-                {props.name}
-            </span>
-            <Counter />
-        </div>
-    );
-};
+import React, {Component} from 'react';
+import Header from './Header';
+import Player from './Player';
 
 
 //!Functional Component
@@ -34,50 +14,8 @@ const Player = (props) => {
 //     )
 // }
 
-class Counter extends React.Component{
-    
-    //! One way to declare state
-    // constructor(){
-    //     super()
-    //     this.state = {
-    //         score: 0
-    //     }
-    // }
-    //! OR
 
-    state = {
-        score: 0
-    };
-
-    incrementScore = () => {
-        this.setState( prevState => {
-            return {
-                score: prevState.score + 1
-            }
-        })
-    }
-
-    decrementScore = () => {
-        this.setState( prevState => {
-            return{
-                score: prevState.score -1
-            }
-        })
-    }
-
-    render(){
-        return(
-            <div className='counter'>
-                <button className='counter-action decrement' onClick={this.decrementScore}> - </button>
-                <span className='counter-score'> {this.state.score} </span>
-                <button className='counter-action increment' onClick={this.incrementScore}> + </button>
-            </div>
-        )
-    }
-}
-
-
-class App extends React.Component {
+class App extends Component {
     state = {
         players: [
         {
